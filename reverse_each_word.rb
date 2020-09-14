60533ff2c2c34908c3dd93a5a7399b36837522e3
Terranie Sims <terranie67@gmail.com>
2020-09-14 01:35:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
-def reverse_each_word (sentence)
-  new_sentence = sentence.split 
-  sentence.collect do |string|
-    new_sentence.reverse 
-  end 
-end 
+def reverse_each_word_with_each(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
